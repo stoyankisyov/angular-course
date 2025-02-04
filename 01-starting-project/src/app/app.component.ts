24d7d3c51ad6b7663740370d6e11dd882c096242
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-02-04 12:38:01 +0200

    Introduce tasks component and display selected user's name

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './user/dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+import { User } from './user/user.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUser: User | undefined;
+
+  onSelectedUser(id: string) {
+    this.selectedUser = this.users.find((user) => user.id === id);
+  }
+}
