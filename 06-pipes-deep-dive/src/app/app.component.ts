efd1d9b0ecab41a9cc6f560481f139a101c54598
Stoyan Kisyov <work.stoyan.kisyov@gmail.com>
2025-02-23 21:46:23 +0200

    Final

--- 06-pipes-deep-dive/src/app/app.component.ts
+++ 06-pipes-deep-dive/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { DatePipe, DecimalPipe } from '@angular/common';
+
+import { TemperaturePipe } from './temperature.pipe';
+import { SortPipe } from './sort.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [DatePipe, DecimalPipe, TemperaturePipe, SortPipe],
 })
 export class AppComponent {
   currentDate = new Date();
@@ -19,6 +24,8 @@ export class AppComponent {
   ];
 
   onReset(index: number) {
-    this.historicTemperatures[index] = 18;
+    const newTemp = [...this.historicTemperatures];
+    newTemp[index] = 18;
+    this.historicTemperatures = newTemp;
   }
 }
