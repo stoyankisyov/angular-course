88aa59f37972e50582aa39e5cc6d100df39d7fcc
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-02-24 13:38:08 +0200

    Introduce Task Status Options values injected to components

--- 07-services-deep-dive/src/app/tasks/tasks-list/task-item/task-item.component.ts
+++ 07-services-deep-dive/src/app/tasks/tasks-list/task-item/task-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { Task, TaskStatus } from '../../task.model';
+import { Task, TASK_STATUS_OPTIONS, TaskStatus } from '../../task.model';
 import { TasksService } from '../../tasks.service';
 
 @Component({
@@ -12,7 +12,8 @@ import { TasksService } from '../../tasks.service';
   styleUrl: './task-item.component.css',
 })
 export class TaskItemComponent {
-  tasksService = inject(TasksService);
+  private tasksService = inject(TasksService);
+  taskStatusOptions = inject(TASK_STATUS_OPTIONS);
   task = input.required<Task>();
   taskStatus = computed(() => {
     switch (this.task().status) {
