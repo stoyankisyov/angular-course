9f123d05263d222826a147572c114a9d1c071565
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-03-15 22:23:50 +0200

    Add custom group validator

--- 11-forms/src/app/auth/signup/signup.component.ts
+++ 11-forms/src/app/auth/signup/signup.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import {
+  AbstractControl,
   FormArray,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+
+function equalValues(controlName1: string, controlName2: string) {
+  return (control: AbstractControl) => {
+    const val1 = control.get(controlName1)?.value;
+    const val2 = control.get(controlName2)?.value;
+
+    if (val1 === val2) {
+      return null;
+    }
+
+    return { valuesNotEqueal: true };
+  };
+}
 
 @Component({
   selector: 'app-signup',
@@ -19,14 +33,19 @@ export class SignupComponent {
     email: new FormControl('', {
       validators: [Validators.required, Validators.email],
     }),
-    passwords: new FormGroup({
-      password: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(6)],
-      }),
-      confirmPassword: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(6)],
-      }),
-    }),
+    passwords: new FormGroup(
+      {
+        password: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(6)],
+        }),
+        confirmPassword: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(6)],
+        }),
+      },
+      {
+        validators: [equalValues('password', 'confirmPassword')],
+      }
+    ),
     firstName: new FormControl('', {
       validators: [Validators.required],
     }),
@@ -62,6 +81,11 @@ export class SignupComponent {
     }),
   });
   onSubmit() {
+    if (this.form.invalid) {
+      console.log('Form is invalid');
+      return;
+    }
+
     console.log(this.form);
   }
 
