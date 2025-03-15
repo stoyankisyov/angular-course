4490b8982bb88053f4dd073ed855f73c7db7e549
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-03-15 21:09:06 +0200

    Add build-in and custom validators

--- 11-forms/src/app/auth/login-reactive-forms/login-reactive-forms.component.ts
+++ 11-forms/src/app/auth/login-reactive-forms/login-reactive-forms.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  }
+
+  return { doesNotContainQuestionMark: true };
+}
 
 @Component({
   selector: 'app-login-reactive-forms',
@@ -10,14 +24,39 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class LoginReactiveFormsComponent {
   form = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        mustContainQuestionMark,
+      ],
+    }),
   });
+
+  get emailIsInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+
+  get passwordIsInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
 
   onSubmit() {
     console.log(this.form);
     const enteredEmail = this.form.value.email;
     const enteredPassword = this.form.value.password;
+
     console.log(
       `Entered Email: ${enteredEmail}, Entered Password: ${enteredPassword}`
     );
