f93fa80e5ee808ea9a7c5c68486128f5788c27cc
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-03-15 21:24:38 +0200

    Load email from localStorage

--- 11-forms/src/app/auth/login-reactive-forms/login-reactive-forms.component.ts
+++ 11-forms/src/app/auth/login-reactive-forms/login-reactive-forms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import {
   AbstractControl,
   FormControl,
@@ -6,7 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { of } from 'rxjs';
+import { debounce, debounceTime, of } from 'rxjs';
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
@@ -31,7 +31,9 @@ function emailIsUnique(control: AbstractControl) {
   templateUrl: './login-reactive-forms.component.html',
   styleUrl: './login-reactive-forms.component.css',
 })
-export class LoginReactiveFormsComponent {
+export class LoginReactiveFormsComponent implements OnInit {
+  private destoryRef = inject(DestroyRef);
+
   form = new FormGroup({
     email: new FormControl('', {
       validators: [Validators.required, Validators.email],
@@ -60,6 +62,31 @@ export class LoginReactiveFormsComponent {
       this.form.controls.password.dirty &&
       this.form.controls.password.invalid
     );
+  }
+
+  ngOnInit() {
+    const savedForm = window.localStorage.getItem('saved-login-form');
+    if (savedForm) {
+      const loadedForm = JSON.parse(savedForm);
+      this.form.patchValue({
+        email: loadedForm.email,
+      });
+    }
+
+    const subscription = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe({
+        next: (value) => {
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.email })
+          );
+        },
+      });
+
+    this.destoryRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 
   onSubmit() {
