f37058ebe694e6b9a0c34a70d5cf50916082835e
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-03-15 21:53:32 +0200

    Connect and register inputs for the form

--- 11-forms/src/app/app.component.ts
+++ 11-forms/src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 
 import { LoginTemplateDrivenComponent } from './auth/login-template-driven/login-template-driven.component';
 import { LoginReactiveFormsComponent } from './auth/login-reactive-forms/login-reactive-forms.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [LoginTemplateDrivenComponent, LoginReactiveFormsComponent],
+  imports: [
+    LoginTemplateDrivenComponent,
+    LoginReactiveFormsComponent,
+    SignupComponent,
+  ],
 })
 export class AppComponent {}
