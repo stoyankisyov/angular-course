7cb99d265736a2976ccb3604c9560027908114b0
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-03-15 19:17:39 +0200

    Connect the form to template

--- 11-forms/src/app/auth/login-reactive-forms/login-reactive-forms.component.ts
+++ 11-forms/src/app/auth/login-reactive-forms/login-reactive-forms.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-reactive-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-reactive-forms.component.html',
-  styleUrl: './login-reactive-forms.component.css'
+  styleUrl: './login-reactive-forms.component.css',
 })
 export class LoginReactiveFormsComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  onSubmit() {}
 }
