831267846c2036c4173025abf013b6973ffff4c3
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-03-16 21:06:39 +0200

    Introduce relative link

--- 12-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 12-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,11 +1,11 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { UsersService } from '../users.service';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
