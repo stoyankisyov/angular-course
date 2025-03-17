805452d3e463f2dd131665482cc087e1a55ea55b
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-03-17 20:38:46 +0200

    Separate routes in external file

--- 12-routing/src/app/app.routes.ts
+++ 12-routing/src/app/app.routes.ts
@@ -2,9 +2,8 @@ import { Routes } from '@angular/router';
 
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { routes as userRoutes } from './users/user.routes';
 
 export const routes: Routes = [
   {
@@ -14,21 +13,7 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'full',
-      },
-      {
-        path: 'tasks',
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-      },
-    ],
+    children: userRoutes,
   },
   {
     path: '**',
